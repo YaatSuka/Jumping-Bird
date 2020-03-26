1c2934f6fecb9a54330e1a6696c137b1f80a7fea
Tom Chitty <tom.chitty@epitech.eu>
2020-03-26 18:20:42 +0100

    manage pipes moving vertically and horizontally

---
+++ Assets/Scripts/Obstacles/ObstaclesMoving.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstaclesMoving : MonoBehaviour
+{
+    public bool isMovingVertically = false;
+    public bool isMovingHorizontally = false;
+    public float yMin = - 2f;
+    public float yMax = 2f;
+    public float xMargin = 3.5f;
+
+    private int sens = 1;
+    private GameObject player;
+    private Transform nextPipe = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+        StartCoroutine("SetNextPipe");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isMovingVertically) {
+            MoveVertically();
+        }
+        else if (isMovingHorizontally) {
+            MoveHorizontally();
+        }
+    }
+
+    private void MoveVertically()
+    {
+        if (transform.localPosition.y <= yMin || transform.localPosition.y >= yMax) {
+            sens *= -1;
+        }
+
+        if (sens == 1) {
+            transform.Translate(Vector2.up * Time.deltaTime, Space.World);
+        } else {
+            transform.Translate(Vector2.down * Time.deltaTime, Space.World);
+        }
+    }
+
+    private void MoveHorizontally()
+    {
+        if (!nextPipe) {
+            return;
+        }
+
+        if (transform.position.x >= nextPipe.position.x - xMargin) {
+            isMovingHorizontally = false;
+        }
+        else if (transform.position.x <= player.transform.position.x) {
+            transform.position = new Vector3(player.transform.position.x, transform.position.y, transform.position.z);
+        }
+    }
+
+    private IEnumerator SetNextPipe() {
+        while (!(nextPipe = GetNextPipe())) {
+            yield return new WaitForSeconds(0.5f);
+        }
+        yield return null;
+    }
+
+    private Transform GetNextPipe()
+    {
+        Transform self = transform;
+        Transform parent = self.parent;
+        for (int i = 0; i < parent.childCount - 1; i++) {
+            if (parent.GetChild(i) == self && parent.GetChild(i + 1)) {
+                return parent.GetChild(i + 1);
+            }
+        }
+        return null;
+    }
+}
--- Assets/Scripts/Obstacles/ProceduralGeneration/PipeGeneration.cs
+++ Assets/Scripts/Obstacles/ProceduralGeneration/PipeGeneration.cs
@@ -10,11 +10,30 @@ public class PipeGeneration : MonoBehaviour
     public float xOffset =  4.5f;
 
     private GameObject parent;
+    private float timer = 0f;
+
+    private IDictionary<float, int> movingVerticalRates = new Dictionary<float, int>();
+    private IDictionary<float, int> movingHorizontalRates = new Dictionary<float, int>();
 
     // Start is called before the first frame update
     void Start()
     {
         parent = GameObject.Find("Obstacles");
+
+        movingVerticalRates[0f] = 10;
+        movingVerticalRates[20f] = 8;
+        movingVerticalRates[30f] = 5;
+        movingVerticalRates[60f] = 2;
+
+        movingHorizontalRates[0f] = 30;
+        movingHorizontalRates[20f] = 10;
+        movingHorizontalRates[45f] = 8;
+        movingHorizontalRates[60f] = 5;
+    }
+
+    void Update()
+    {
+        timer += Time.deltaTime;
     }
 
     void OnTriggerEnter2D(Collider2D collision)
@@ -24,7 +43,33 @@ public class PipeGeneration : MonoBehaviour
             float y = transform.position.y + Random.Range(-yPadding, yPadding);
             Vector3 position = new Vector3(x, y, -15f);
 
-            Instantiate(pipe, position, Quaternion.identity, parent.transform);
+            GameObject obstacle = Instantiate(pipe, position, Quaternion.identity, parent.transform);
+
+            // Vertical move
+            float i = 0f;
+            foreach(KeyValuePair<float, int> rate in movingVerticalRates) {
+                if (timer >= rate.Key) {
+                    i = rate.Key;
+                }
+            }
+
+            int moveVertically = Random.Range(0, movingVerticalRates[i] + 1);
+            if (moveVertically == 0) {
+                obstacle.GetComponent<ObstaclesMoving>().isMovingVertically = true;
+            }
+
+            // Horizontal move
+            float j = 0f;
+            foreach(KeyValuePair<float, int> rate in movingHorizontalRates) {
+                if (timer >= rate.Key) {
+                    j = rate.Key;
+                }
+            }
+
+            int moveHorizontally = Random.Range(0, movingHorizontalRates[j] + 1);
+            if (moveHorizontally == 0) {
+                obstacle.GetComponent<ObstaclesMoving>().isMovingHorizontally = true;
+            }
         }
     }
 }
